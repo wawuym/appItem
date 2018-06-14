d5b7d68da14c29c56ec9915d06b4d6a79d27775c
heterosex <jialin571416094@qq.com>
2018-06-14 18:02:11 +0800

    头部和尾部和路路由

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,98 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 头部  -->
+    <header>{{title}}</header>
+
+    <!-- 通过路由来点击添加 的内容 -->
     <router-view/>
+
+    <!-- 尾部 -->
+    <footer>
+      <div class="bottom">
+      <ul>
+        <router-link tag= "li" to = "/home" exact> 首页  </router-link>
+        <router-link tag= "li" to = "/classify">分类</router-link>
+        <router-link tag= "li" to = "shopping">购物车</router-link>
+        <router-link tag= "li" to = "person">我</router-link>
+      </ul>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      title:"首页",
+      navIem:["首页","分类","购物车","wo"]
+    }
+  },
+  
 }
 </script>
 
 <style lang="scss">
-html,body {
-    font: normal 14px "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin: 0;
+// 公共的样式
+*{
+ box-sizing: border-box; 
 }
+html,body {
+  height: 100%;
+  font: normal 16px "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+}
+#app{
+  height:100%;
+}
+ul{
+  margin: 0;
+  padding: 0;
+}
+ul li{
+  list-style: none;
+}
+//内容的样式
+.content{
+  height:calc(100% - 100px);
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+// 头部的样式
+header{
+  height:50px;
+  background:#000;
+  color:white;
+  line-height:50px;
+  text-align: center
+}
+//尾部的样式
+footer{
+  height:50px;
+}
+footer .bottom{
+  background:#000;
+  color:white;
+}
+footer .bottom ul{
+  display: flex
+}
+footer .bottom ul li{
+  width:24%;
+  justify-content: space-between;
+  line-height:50px;
+  text-align: center;
+}
+
+
 </style>
+
+
+
+
+
---
+++ src/page/classify/Classify.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content">
+    <p>我是classify</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'classify',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped >
+
+
+
+</style>
