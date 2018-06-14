602fa1420752ca03d14629ae7f09b239fe3cd255
wawuym <867044349@qq.com>
2018-06-14 13:16:12 +0800

    创建了项目结构

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+html,body {
+    font: normal 14px "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin: 0;
+}
+</style>
