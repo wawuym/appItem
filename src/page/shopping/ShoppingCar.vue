29c7dd38af23104903458352d6bf59d003f1f1ef
wawuym <867044349@qq.com>
2018-06-18 16:42:56 +0800

    完成了购物车数量加减及选中状态

--- src/page/shopping/ShoppingCar.vue
+++ src/page/shopping/ShoppingCar.vue
@@ -9,24 +9,24 @@
     </td></tr></tbody></table> 
     <!-- 有商品时  -->
     <div v-else>
-       <div class="edit" @click="edit">编辑</div>
+       <div class="edit"><span  @click="edits">{{edit}}</span></div>
        <div class="flow-list">
          <ul>
            <li class="check" v-if='check'>   
-              <span class="no" v-if='check'></span>           
+              <span class="no" v-if='checkMe' @click="changeClir"></span>           
               <span class="yes" v-else></span>
            </li>
            <li class="pic"><img src="https://www.amflower.com/images/flowers/62741-big.jpg" alt=""></li>
            <li class="detail">
              <h6>缤纷设计鲜花系列</h6>
              <p class="useFlower">爱情鲜花，周年纪念，求婚鲜花，生日鲜花</p>
-             <span class="price">￥109</span>
+             <span class="price" >￥{{getPrices}}</span>
            </li>
 
-             <div class="layui-btn-group layui-btn-size" v-if='jisun'>
-                <button class="layui-btn layui-btn-primary layui-btn-xs">—</button>
-                <button class="layui-btn layui-btn-primary layui-btn-xs">1</button>
-                <button class="layui-btn layui-btn-primary layui-btn-xs"><i class="layui-icon">&#xe654;</i></button>
+             <div class="layui-btn-group layui-btn-size" v-if='!check'>
+                <button class="layui-btn layui-btn-primary layui-btn-xs" @click='desc' :disabled='yes'>—</button>
+                <input type="button" class="layui-btn layui-btn-primary layui-btn-xs" v-model='count' readonly>
+                <button class="layui-btn layui-btn-primary layui-btn-xs" @click='add'><i class="layui-icon">&#xe654;</i></button>
              </div>  
          </ul>    
        </div>
