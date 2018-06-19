7910e43e6f857afbb3ad7f68b85f0b636a92ea6e
heterosex <jialin571416094@qq.com>
2018-06-19 14:07:56 +0800

    添加了一个home里面的matter内容

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="content">
-    <p>我是home</p>
+    <!-- <p>我是home 轮播图</p> -->
+    <!-- <axios></axios> -->
+    <matter :dataJson = dataJson ></matter>
   </div>
 </template>
 
 <script>
+  import Axios from '../axios/axios'
+  import Matter from '../home/Matter'
+
 export default {
   name: 'home',
+  //数据
   data () {
     return {
-     
+     dataJson:[],
     }
+     
+  },
+  //获取json 数据 ，并从父级 传给 子级
+  created(){
+   this.$axios.get("static/data/flowerData.json")
+    .then( resp => {
+      console.log(resp.data)
+      this.dataJson=resp.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  },
+
+  //计算 函数 里面写 传入的组件名
+  components:{
+    Axios,
+    Matter
+  },
+
+  methods:{
   }
 }
 </script>
+
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped >
