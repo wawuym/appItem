227ff6be1f03c927d81b66c54e71a446a33437fe
wawuym <867044349@qq.com>
2018-06-19 18:38:30 +0800

    余删除了自己写的分类文件

--- src/page/classify/Classify.vue
+++ src/page/classify/Classify.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="content">
-    <div class="research">
-      <i class="layui-icon researchIcon">&#xe615;</i>
-      <input type="text" required  lay-verify="required" placeholder="请输入你要查找的鲜花名称" autocomplete="off" class="layui-input searchInput">   
-    </div>
-    <ul class="searchList">
-      <li class="pic"><img src="https://www.amflower.com/images/flowers-feature/62818-c.jpg" alt=""></li>
-      <li class="detail">
-        <p>弗郎花瓶</p>
-        <p class="useFlower">爱情鲜花，周年纪念</p>
-        <p class="price">109</p>
-      </li>
-      <li>
-       <i class="layui-icon">&#xe602;</i>   
-      </li>
-    </ul>
+   
   </div>
  
 </template>
@@ -24,13 +10,8 @@ export default {
   name: 'classify',
   data () {
     return {
-      flowerArr:[]
+     
     }
-  },
-  created(){
-    axios.get('static/data/flowerData.json?').then((res)=>{
-        this.flowerArr=res.data;
-    })
   }
 }
 </script>
@@ -38,6 +19,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped >
 
-@import './classify.scss'
 
 </style>
